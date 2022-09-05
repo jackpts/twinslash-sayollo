d6382973822b2868a050a085cc1626bec2db7e20
Eugene Pavlukanets <eugeny.oya@gmail.com>
2022-09-05 18:42:05 +0300

    twinslash - BreakpointObserver added

--- twinslash-sayollo/src/app/app.component.ts
+++ twinslash-sayollo/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'twinslash-sayollo';
+  isMobileView: boolean = false;
+
+  constructor(private responsive: BreakpointObserver) {
+  }
+
+  ngOnInit(): void {
+
+    this.responsive.observe([
+      Breakpoints.Handset,
+      Breakpoints.Tablet,
+      Breakpoints.TabletPortrait,
+      Breakpoints.HandsetLandscape,
+    ])
+      .subscribe(result => {
+
+        if (result.matches) {
+          this.isMobileView = true;
+        }
+      });
+  }
 }
